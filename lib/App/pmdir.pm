ec1df19baf72b46883444ace9a56c54451bd170b
Torsten Raudssus <torsten@raudss.us>
2014-09-15 21:47:14 +0200

    First release

--- lib/App/pmdir.pm
+++ lib/App/pmdir.pm
@@ -1,5 +1,45 @@
+package App::pmdir;
+# ABSTRACT: returns the directory of a specific Perl module
+
 use strict;
 use warnings;
-package App::pmdir;
+use Module::Runtime qw( require_module );
+use File::Spec;
+
+sub pmdir {
+  my ( $module ) = @_;
+  my $dir;
+  eval {
+    require_module($module);
+    my $filename = $module;
+    $filename =~ s!::!/!g;
+    $filename .= '.pm';
+    my $file = $INC{$filename};
+    return undef unless $file;
+    my @return = File::Spec->splitpath($file);
+    $dir = $return[1];
+  };
+  return undef if $@ || !$dir;
+  return $dir;
+}
 
 1;
+
+=head1 DESCRIPTION
+
+See L<pmdir>.
+
+=head1 SUPPORT
+
+IRC
+
+  Join #sycontent on irc.perl.org. Highlight Getty for fast reaction :).
+
+Repository
+
+  https://github.com/Getty/p5-app-pmdir
+  Pull request and additional contributors are welcome
+ 
+Issue Tracker
+
+  https://github.com/Getty/p5-app-pmdir/issues
